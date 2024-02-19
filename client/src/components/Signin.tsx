495d74fc7ba590b7383339565f4c4c845363469b
Gaurav P. Malpedi <gaurav.malpedi17@gmail.com>
2024-02-19 15:49:42 +0530

    changes

--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -140,7 +140,7 @@ function Signin() {
         style={{fontFamily: 'Cookie', fontSize: "20px", color: 'white'}}
         className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" 
         onClick={async () => {
-            const res = await axios.post("http://localhost:3000/auth/login", {
+            const res = await axios.post("/auth/login", {
                 username: email,
                 password: password
             }, {
